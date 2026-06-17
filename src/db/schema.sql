9ce855ae78cbb03090241cb893343da2d2cfa26d
Gabriel Thornburgh <gabriel.thornburgh1@gmail.com>
2026-06-17 11:26:32 -0400

    inital commit

---
+++ src/db/schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+
+    firstName VARCHAR(25) NOT NULL,
+    lastName VARCHAR(25) NOT NULL,
+
+    email VARCHAR(75) UNIQUE NOT NULL,
+
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+
+    userIds INTEGER[] DEFAULT '{}', 
+    owner_id INTEGER NOT NULL REFERENCES users(id),
+
+    inviteCode VARCHAR(25) NOT NULL UNIQUE,
+    
+    confirmedEventIds INTEGER[] DEFAULT '{}'
+);
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    groupId INTEGER NOT NULL REFERENCES groups(id),
+
+    title VARCHAR NOT NULL,
+
+    startDate DATE,
+    endDate DATE,
+
+    googleMapsApiId VARCHAR,
+    ticketmasterId VARCHAR,
+
+    eventImageUrl TEXT,
+    description TEXT,
+
+    votesFor INTEGER[] DEFAULT '{}',
+    votesAgainst INTEGER[] DEFAULT '{}',
+
+    votingEnds DATE,
+
+    confirmed BOOLEAN DEFAULT FALSE
+);
